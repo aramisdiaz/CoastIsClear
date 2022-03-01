2b6989304207939eeefb16403d64c09ce4bcfde4
Aramis Diaz <aramisdiaz@mail.usf.edu>
2022-02-28 21:08:17 -0800

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,7 +51,7 @@ $(window).on("scroll", function() {
 
   // Here we run our AJAX call to the Google Places API
 
-  var MAPqueryURL = "https://cors.io/?https://maps.googleapis.com/maps/api/place/textsearch/json?query=beaches+in+" + place + "&key=" + mapAPIKey;
+  var MAPqueryURL = "http://cors.io/?https://maps.googleapis.com/maps/api/place/textsearch/json?query=beaches+in+" + place + "&key=" + mapAPIKey;
 
   $.ajax({
     url: MAPqueryURL,
